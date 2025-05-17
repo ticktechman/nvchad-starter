03686b028fbc44971d1b4df36cb5b2eef9135280
ticktechman <ticktechman@gmail.com>
2025-05-17 18:19:14 +0800

    first commit for ticktechman

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,23 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-----------------------------------------------
+-- added for ticktechman
+map("n", "<leader>z", ":q!<CR>")
+
+-- tabufline
+map("n", "L", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+map("n", "H", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto next" })
+
+-- nvimtree
+map("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- tagbar
+map("n", "<leader>t", ":TagbarToggle<CR>")
+map("n", "<leader>,", ":call QuickFixToggle()<CR>")
+-----------------------------------------------
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,17 @@ return {
     end,
   },
 
+  -- added by ticktechman
+  {
+    "majutsushi/tagbar",
+    lazy = false,
+    config = function()
+      vim.g.tagbar_sort = 0
+    end,
+  },
+  { "godlygeek/tabular", lazy = false },
+  { "ntpeters/vim-better-whitespace", lazy = false },
+  { "vim-scripts/a.vim", lazy = false },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
